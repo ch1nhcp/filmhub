a643ee73058516967493b47250fb8e7b58f01b71
haidang99765 <74364083+haidang99765@users.noreply.github.com>
2021-03-20 16:56:22 +0700

    menu update

--- components/menu.js
+++ components/menu.js
@@ -1,17 +1,19 @@
 const $template = document.createElement('template');
 $template.innerHTML = /*html*/`
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-<div class="row container-fluid">
+<div class="container-fluid">
+<div class="row bg-dark">
     <div class="d-inline-flex justify-content-left">
         <nav class="navbar navbar-expand-sm mt-2">
-            <a class="navbar-brand" href="#">Filmhub</a>
+            <a class="navbar-brand text-danger" href="#">Filmhub</a>
         </nav>
     </div>
     <div class="d-inline-flex justify-content-end col mt-3">
-        <a class="navbar-brand" href="#">Contact</a>
-        <a class="navbar-brand" href="#">Đăng kí</a>
-        <a class="navbar-brand" href="#">Đăng nhập</a>
+        <a class="navbar-brand text-white" href="#">Contact</a>
+        <a class="navbar-brand text-white" href="#">Đăng kí</a>
+        <a class="navbar-brand text-white" href="#">Đăng nhập</a>
     </div>
+</div>
 </div>
 `;
 
